6adee2f15f706953a75243441fab71b4fca7eedd
snehamore23 <moresneha806@gmail.com>
2026-07-08 13:03:47 +0530

    api created

--- server/index.js
+++ server/index.js
@@ -19,7 +19,14 @@ const connectDB = async () => {
     }
 };
 
-const PORT = process.env.PORT || 8080;
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Server is up and running..."
+    });
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
