3d9c4e9cd117b1b08ef71ddae49ec4a13b20261d
snehamore23 <moresneha806@gmail.com>
2026-07-08 12:44:40 +0530

    connect mongodb successfull

--- server/index.js
+++ server/index.js
@@ -2,6 +2,26 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+
 dotenv.config();
 
-const app=express();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("✅ MongoDB Connected");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+    await connectDB();
+});
