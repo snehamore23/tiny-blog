b743bb671cded31d8ddb32f3eff0c7e8f7967ea5
snehamore23 <moresneha806@gmail.com>
2026-09-09 18:05:55 +0530

    client libraries

--- client/vite-project/src/main.jsx
+++ client/vite-project/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import AppBlogs from "./views/AllBlogs";
+import NewBlog from "./views/NewBlog";
+import EditBlog from "./views/EditBlog";
+import ReadBlog from "./views/ReadBlogs";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AppBlogs />} />
+      <Route path="/new" element={<NewBlog />} />
+      <Route path="/edit/:id" element={<EditBlog />} />
+      <Route path="/blog/:slug" element={<ReadBlog />} />
+
+      <Route
+        path="*"
+        element={
+          <h1 className="text-center mt-5">
+            404 Not Found
+          </h1>
+        }
+      />
+    </Routes>
+  </BrowserRouter>
+);
