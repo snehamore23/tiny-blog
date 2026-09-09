ee8b47d5504ab40ce04f5fe98787221d9d1e6b76
snehamore23 <moresneha806@gmail.com>
2026-09-09 14:59:47 +0530

    signup page

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,10 +2,72 @@ import User from "../models/User.js";
 
 const postSignup = async (req, res) => {
     try {
-        const { username, email, password } = req.body;
+        const { name, email, password } = req.body;
+
+        if (!name || !email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Name, email, and password are required"
+            });
+        }
+
+        console.log(name, email, password); // Log the request body for debugging
+
+        const emailValidationRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const nameValidationRegex = /^[a-zA-Z]+$/;
+        const passwordValidationRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        if (emailValidationRegex.test(email) === false) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email format"
+            });
+        }
+
+        if (nameValidationRegex.test(name) === false) {
+            return res.status(400).json({
+                success: false,
+                message: "Name should contain only letters"
+            });
+        }
+
+        if (passwordValidationRegex.test(password) === false) {
+            return res.status(400).json({
+                success: false,
+                message: "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+            });
+        }
+
+        if (!name) {
+            return res.status(400).json({
+                success: false,
+                message: "Name is required"
+            });
+        }
+
+        if (!email) {
+            return res.status(400).json({
+                success: false,
+                message: "Email is required"
+            });
+        }
+
+        if (!password) {
+            return res.status(400).json({
+                success: false,
+                message: "Password is required"
+            });
+        };
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: `User with email ${email} already exists`
+            });
+        }
 
         const newUser = new User({
-            name: username,
+            name,
             email,
             password
         });
