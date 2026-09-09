aa2336e5356e661efd06a75716638f6442a40ce9
snehamore23 <moresneha806@gmail.com>
2026-09-09 22:14:28 +0530

    login and signup page created

--- client/vite-project/src/main.jsx
+++ client/vite-project/src/main.jsx
@@ -6,6 +6,8 @@ import AppBlogs from "./views/AllBlogs";
 import NewBlog from "./views/NewBlog";
 import EditBlog from "./views/EditBlog";
 import ReadBlog from "./views/ReadBlogs";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -14,6 +16,9 @@ createRoot(document.getElementById("root")).render(
       <Route path="/new" element={<NewBlog />} />
       <Route path="/edit/:id" element={<EditBlog />} />
       <Route path="/blog/:slug" element={<ReadBlog />} />
+      <Route path="/Login" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+
 
       <Route
         path="*"
