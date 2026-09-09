ae15611717de0d1bbab538aa1315cc7a55591235
snehamore23 <moresneha806@gmail.com>
2026-09-09 16:10:33 +0530

    login created

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -88,14 +88,37 @@ const postSignup = async (req, res) => {
     }
 };
 
-const postLogin = (req, res) => {
+const postLogin = async (req, res) => {
     const { email, password } = req.body;
 
-    res.json({
-        success: true,
-        message: "Login API working",
-        email: email
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: "Email and password are required"
+        });
+    }
+
+    try {
+        const existingUser = await User.findOne({ email, password });
+    if (existingUser) {
+        return res.json({
+            success: true,
+            message: "User logged in successfully",
+            user: existingUser,
+
+        });
+    }else{
+        return res.status(401).json({
+            success: false,
+            message: "Invalid email or password"
+        });
+    }
+} catch (error) {
+    return res.status(500).json({
+        success: false,
+        message: error.message
     });
+}
 };
 
 export { postSignup, postLogin };
