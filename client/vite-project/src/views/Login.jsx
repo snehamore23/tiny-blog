8efebfb3944e1641878a2a5de39a136120d2bc0e
snehamore23 <moresneha806@gmail.com>
2026-09-10 22:58:59 +0530

    fix login component

--- client/vite-project/src/views/Login.jsx
+++ client/vite-project/src/views/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
   const loginUser = async () => {
     try {
       const response = await axios.post(
-        "http://localhost:5000/login",
+        `${import.meta.env.VITE_API_URL}/login`,
         user
       );
 
