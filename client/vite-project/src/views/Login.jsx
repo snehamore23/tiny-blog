de476292cccbbdee34ebfd8d601eb890474355b0
snehamore23 <moresneha806@gmail.com>
2026-09-10 21:36:41 +0530

    done with login and signup page

--- client/vite-project/src/views/Login.jsx
+++ client/vite-project/src/views/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
   const loginUser = async () => {
     try {
       const response = await axios.post(
-        `${import.meta.env.VITE_API_URL}/login`,
+        "http://localhost:5000/login",
         user
       );
 
